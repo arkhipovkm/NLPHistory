de049f948591ff59f340d96609500f7cf039f08a
arkhipovkm <arkhipov.km@gmail.com>
2018-03-11 23:04:15 +0100

    minor

--- acquire_comments.py
+++ acquire_comments.py
@@ -40,7 +40,7 @@ def vkapi(func):
     def inner(*args, **kwargs):
         params = func(*args, **kwargs)
         counter = 0
-        while counter < 3:
+        while counter < 2:
             url = URL + params #+ 'access_token={}'.format(token)
             resp = requests.get(url).json()
             try:
@@ -60,7 +60,7 @@ def vkapi(func):
                     print('Hit the TooManyOperationsError. Sleep 1 sec and continue/ Retry: {}'.format(counter))
                     from re import sub
                     params = sub('&req=[0-9]{2}&', '&req=10&', params)
-                    sleep(1)
+                    #sleep(1)
                     counter += 1
                     continue
                 else:
@@ -146,7 +146,7 @@ def main_stored(group):
     while offset/posts_count < 0.60:
         response = execute_get_comments(group, offset=offset)
         if not response:
-            offset += 10
+            offset += 20
             print('Skipped request..')
             continue
             #break
