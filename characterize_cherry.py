f5bd5d1a8de429fb0af0d6f4564e12b687ea569e
arkhipovkm <arkhipov.km@gmail.com>
2018-04-27 10:39:47 +0200

    Polarity onload

--- characterize_cherry.py
+++ characterize_cherry.py
@@ -153,6 +153,61 @@ class Characterize():
 
         return json.dumps(obj).encode('utf-8')
 
+
+
+    @expose
+    def get_rubric_polarity(self, rubric_id=None):
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        rubric_id = int(rubric_id)
+        #ddf = df[df.rubric_id==rubric_id].sort_values(by=['likes'], ascending=False).drop_duplicates()
+        ddf = df[df.rubric_id==rubric_id].drop_duplicates()
+        #print(len(ddf))
+        ddf['age'] = (ddf.date - ddf.bdate).dt.days/365
+        ddf['current'] = [x+1 for x in range(len(ddf))]
+        group_name = ddf.group.unique()[0]
+        max_likes = int(max(ddf.likes))
+        mean_likes = int(np.mean(ddf.likes))
+
+        total_raw = len(ddf)
+
+        with DB() as db:
+            deleted = db.custom_get('select comment_id from id_rubric where rubric_id=%s and is_deleted=1', (rubric_id, ))
+            deleted = [x[0] for x in deleted]
+            done = db.custom_get('select comment_id from id_rubric where rubric_id=%s and polarity is not null', (rubric_id, ))
+            done = [x[0] for x in done]
+
+            #xars = db.custom_get('select distinct characteristic from id_rubric where rubric_id=%s', (rubric_id, ))
+
+            #xars = [x[0] for x in xars if x[0]]
+
+        ddf = ddf.drop(pd.Series(deleted), axis=0)
+        ddf = ddf.drop(pd.Series(done), axis=0)
+
+        ddf = ddf.reset_index()
+
+        ddf.date = ddf.date.dt.strftime('%Y-%m-%d')
+        ddf = ddf.drop('bdate', axis=1)
+
+        rubric_name = pd.read_pickle('rubric_names.pickle')[rubric_id]
+        #group_name = ddf.group.unique()[0]
+        #ddf_js = ddf.head().to_json(orient='values')
+
+        #whole = json.loads(json.dumps(ddf.to_dict('records')[:300]).encode('utf-8').decode('utf-8', errors='ignore'))
+        whole = ddf[:300].to_json(orient='records')
+
+        obj = {'rubric_id': rubric_id,
+               'rubric_name': rubric_name,
+               'group_name': group_name,
+               'done_count': len(done),
+               'total_count': total_raw,
+               'likes_max': max_likes,
+               'likes_mean': mean_likes,
+               #'xars_list': xars,
+               'whole_data': whole}
+
+        return json.dumps(obj).encode('utf-8')
+
 if __name__ == '__main__':
     df = pd.read_pickle('df_indexed.pickle')
     #print(len(df))
